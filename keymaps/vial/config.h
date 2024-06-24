f2d0d0c01b405bb5725947a1f7cc8ee7c76ab6bc
Catalin Plesu <catalinhimself@gmail.com>
2024-06-24 11:51:13 +0300

    initial commit

---
+++ keymaps/vial/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x3D, 0x9C, 0xF1, 0x88, 0xBC, 0x38, 0x35, 0x91}
+
+//#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+//#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
+
+#define DYNAMIC_KEYMAP_MACRO_COUNT 99
+#define DYNAMIC_KEYMAP_LAYER_COUNT 9
